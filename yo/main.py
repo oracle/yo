oracle/yo

commit fc98dd2d982d543a737f78f976d39a066b4764de

task: add the ability to create tasks at runtime
For scripting use cases, it is nice to be able to put a script into a
string literal, customize it a bit, and then create a task based on
that. Yo normally loads tasks by name out of a list of task directories.
Add the ability to create a task with a name and a script. All the same
conveniences of tasks (e.g. dependencies) are available.

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

yo/main.py
@@ -479,25 +479,7 @@ class YoTask:
     conflicts: t.List[str]
 
     @classmethod
-    @lru_cache(maxsize=None)
-    def load(cls, name: str) -> "YoTask":
-        """
-        Load a task by name and return it.
-
-        This function is cached - it will only really search and load any given
-        task name once: then it will return the same object thereafter.
-        """
-        for directory in TASK_DIRECTORIES:
-            path = os.path.join(directory, name)
-            if os.path.isfile(path):
-                path = os.path.abspath(path)
-                break
-        else:
-            raise YoExc(f"error: Script for task {name} not found")
-
-        with open(path) as f:
-            script = f.read()
-
+    def create_from_string(cls, name: str, script: str) -> "YoTask":
         dependencies = []
         conflicts = []
         all_tasks = list_tasks()
@@ -518,7 +500,30 @@ class YoTask:
                         "MAYBE_DEPENDS_ON", "# MAYBE_DEPENDS_ON"
                     )
                 lines[i] = line
-        return YoTask(name, path, "\n".join(lines), dependencies, conflicts)
+        return YoTask(
+            name, "(memory)", "\n".join(lines), dependencies, conflicts
+        )
+
+    @classmethod
+    @lru_cache(maxsize=None)
+    def load(cls, name: str) -> "YoTask":
+        """
+        Load a task by name and return it.
+
+        This function is cached - it will only really search and load any given
+        task name once: then it will return the same object thereafter.
+        """
+        for directory in TASK_DIRECTORIES:
+            path = os.path.join(directory, name)
+            if os.path.isfile(path):
+                path = os.path.abspath(path)
+                break
+        else:
+            raise YoExc(f"error: Script for task {name} not found")
+
+        with open(path) as f:
+            script = f.read()
+        return cls.create_from_string(name, script)
 
 
 @lru_cache(maxsize=1)

commit 0eb9cd6118ec884f72ec9587a5b7fd67d997e021

Restore the filename of tasks
Since the commit in fixes, tasks have all had the path "(memory)". Add
back in a way to report the path of the script.

Fixes: fc98dd2 ("task: add the ability to create tasks at runtime")

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

yo/main.py
@@ -486,7 +486,9 @@ class YoTask:
     conflicts: t.List[str]
 
     @classmethod
-    def create_from_string(cls, name: str, script: str) -> "YoTask":
+    def create_from_string(
+        cls, name: str, script: str, path: str = "(memory)"
+    ) -> "YoTask":
         dependencies = []
         conflicts = []
         all_tasks = list_tasks()
@@ -507,9 +509,7 @@ class YoTask:
                         "MAYBE_DEPENDS_ON", "# MAYBE_DEPENDS_ON"
                     )
                 lines[i] = line
-        return YoTask(
-            name, "(memory)", "\n".join(lines), dependencies, conflicts
-        )
+        return YoTask(name, path, "\n".join(lines), dependencies, conflicts)
 
     @classmethod
     @lru_cache(maxsize=None)
@@ -530,7 +530,7 @@ class YoTask:
 
         with open(path) as f:
             script = f.read()
-        return cls.create_from_string(name, script)
+        return cls.create_from_string(name, script, path=path)
 
 
 @lru_cache(maxsize=1)